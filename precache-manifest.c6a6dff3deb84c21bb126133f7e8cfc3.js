c6fb0f4229fc8bbeb732517e153313a826a1120d
kaitlinaclark <kaitlinaclark@wustl.edu>
2019-10-17 12:14:10 -0500

    Updates

---
+++ precache-manifest.c6a6dff3deb84c21bb126133f7e8cfc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0e3f5189260fdc0be1793199fe58f1",
+    "url": "/job-scraper/index.html"
+  },
+  {
+    "revision": "c5a37b26604cc0df7696",
+    "url": "/job-scraper/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7b10ff1cff1214397035",
+    "url": "/job-scraper/static/js/2.cae35a36.chunk.js"
+  },
+  {
+    "revision": "c5a37b26604cc0df7696",
+    "url": "/job-scraper/static/js/main.bc9b152f.chunk.js"
+  },
+  {
+    "revision": "6576e8e6fe0fa61c18af",
+    "url": "/job-scraper/static/js/runtime-main.98d613b1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/job-scraper/static/media/logo.25bf045c.svg"
+  }
+]);
